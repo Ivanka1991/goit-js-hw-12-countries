3a222ff2390683015679333eb43673145448eb7c
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2021-05-24 22:40:57 +0300

    написала код

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'https://restcountries.eu/rest/v2/name';
+
+const fetchCounries = country => fetch(`${BASE_URL}/${country}`).then(response => response.json());
+
+export default { fetchCounries };
---
+++ src/js/search.js
@@ -0,0 +1,63 @@
+import debounce from 'lodash.debounce';
+import API from './fetchCounries.js';
+import countryListTpl from '../templates/countryList.hbs';
+import countryCardTpl from '../templates/countryCard.hbs';
+
+import { info, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+
+const refs = {
+inputSearch: document.querySelector('.input-search'),
+jsCardContainer: document.querySelector('.js-card-container'),
+}
+
+let searchCountry = '';
+
+refs.inputSearch.addEventListener('input',
+debounce(()=>{
+    onSearch();
+},500),
+)
+
+function onSearch(){
+    searchCountry = refs.inputSearch.value;
+    if(searchCountry){
+        API.fetchCountries(searchCountry).then(countryCheck).catch(onFetchError);
+    }
+    if(!searchCountry){
+        clearMarkup();
+    }
+}
+function onFetchError(error){
+    console.log(eror);
+}
+function countryCheck(country){
+    if(country.length > 10){
+        clearMarkup();
+        tooMuch();
+    }else if(country.length <= 10 && country.length >1){
+        clearMarkup();
+        renderCountryCard(countryListTpl, country[0]);
+    }else if(country.length === 1) {
+        clearMarkup();
+        renderCountryCard(countryCardTpl, country[0]);
+    }
+}
+
+function renderCountryCard(template, country){
+    const markup = template(country);
+    console.log(markup);
+    refs.jsCardContainer.innerHTML = markup
+}
+
+function tooMuch() {
+    error({
+        tittle: 'Ахтунг!Ахтунг',
+        text: 'Слишком много совпадений!Введи больше текста!'
+    });
+}
+function clearMarkup(){
+    refs.jsCardContainer.innerHTML = '';
+}
